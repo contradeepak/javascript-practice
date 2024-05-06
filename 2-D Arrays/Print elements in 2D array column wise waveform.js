d1dc9b3b3933a272f94bd34d1fac01b8a4fa63a5
contradeepak <deepaktomss@gmail.com>
2024-05-06 16:34:42 +0530

    Print elements in 2D array column wise waveform

---
+++ 2-D Arrays/Print elements in 2D array column wise waveform.js
@@ -0,0 +1,26 @@
+function print(grid, m, n) {
+    let str = "";
+    for (let col = 0; col < n; col++) {
+        if (col % 2 == 0) {
+            // even col -> up to down
+            for (let row = 0; row < m; row++) {
+                str += grid[row][col] + " ";
+            }
+        } else {
+            // odd col down to up
+            for (let row = m - 1; row >= 0; row--) {
+                str += grid[row][col] + " ";
+            }
+        }
+    }
+    console.log(str);
+}
+
+let grid = [
+    [0, 1, 2, 3],
+    [4, 5, 6, 7],
+    [8, 9, 10, 11],
+    [12, 13, 14, 15]
+];
+
+print(grid, 4, 4);
