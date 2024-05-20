fd662a80d6f7bed0f34e72483d0b4a16f0011723
contradeepak <deepaktomss@gmail.com>
2024-05-20 22:46:38 +0530

    CountingSort

---
+++ Sorting Algorithms/Counting Sort/Stablecountingsort.js
@@ -0,0 +1,26 @@
+function counting_sort(arr){
+    let maxEl = -Infinity;
+    for(let i=0; i<arr.length; i++){
+        maxEl = Math.max(maxEl, arr[i]);
+    }
+    let freq = Array(maxEl+1).fill(0);
+    for(let i=0; i< arr.length; i++){
+        let currElement = arr[i];
+        freq[currElement]++;
+
+    }
+    for(let i =1; i<freq.length; i++){
+        freq[i] = freq[i] + freq[i-1];
+    }
+    console.log(freq);
+
+    let output = Array(arr.length);
+    for(let i=arr.length-1; i>=0; i--) {
+        output[freq[arr[i]] -1 ] = arr[i];
+        freq[arr[i]]--;
+
+    }
+    return output;
+}
+
+console.log(counting_sort([4,6,12,9,65,8,5]))
